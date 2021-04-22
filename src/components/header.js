1001c6e3695b2441ea2ed883e1b6cf6810be8081
surajk95 <surajkukati95@gmail.com>
2021-04-22 20:35:15 +0530

    functional header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import { Link } from "gatsby";
-import React from "react";
+import { navigate } from "@reach/router" ;
+import React, {useEffect} from "react";
 
 const navItems = [
   {title: 'home', link: "/", description: "go back home"},
   {title: 'apps', link: "/sesh", description: "appsu"},
   {title: 'blog', link: "/blog", description: "what's a blog"},
-  {title: 'about', link: "/about", description: "what's s fever trip"},
+  {title: 'about', link: "/about", description: "what's a fever trip"},
 ]
 
+let enableHover = false;
+
 const onNavMouseOver = (id) => {
+  if(!enableHover)
+    return;
   onNavMouseOut();
   const targetNav = document.querySelector(`#nav-${id}`);
   if(targetNav)  targetNav.classList.add('show');
@@ -27,25 +32,54 @@ const onNavMouseOut = () => {
   });
 }
 
+const linkClicked= (link) => {
+  enableHover=false;
+  navigate(link);
+  onNavMouseOut();
+}
+
+const onMouseMove = () => {
+  if(!enableHover) {
+    enableHover = true;
+  }
+}
+
+const getItemDescription = (item) => {
+  if(window.location.pathname === item.link) {
+    return "You are already here";
+  }
+  else {
+    return item.description;
+  }
+}
+
 const Header = (props) => {
   const { siteTitle } = props;
+
+  useEffect(() => {
+    enableHover = false;
+  });
+
   return (
     <header
       className="nav active"
       id="nav"
+      onMouseMove={onMouseMove}
     >
       {
         navItems.map((item, index) => (
           <div className="linkItem" id={`linkItem-${item.title}`} key={index}>
-            <div className="linkDescription" id={`nav-${item.title}`} >{item.description}</div>
-            <Link
-              to={item.link}
+            <div className="linkDescription" id={`nav-${item.title}`} >
+              {getItemDescription(item)}
+            </div>
+            <div
               className="linkTitle"
               onMouseOver={()=>onNavMouseOver(item.title)}
               onMouseOut={onNavMouseOut}
+              onClick={()=>linkClicked(item.link)}
             >
               {item.title}
-            </Link>
+            </div>
           </div>
         ))
       }
