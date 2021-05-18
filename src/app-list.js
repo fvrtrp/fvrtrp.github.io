db230e20030dbea9caca8484ab929b1dbc808e90
surajk95 <surajkukati95@gmail.com>
2021-05-18 12:56:11 +0530

    added player of numbers to app list

--- src/app-list.js
+++ src/app-list.js
@@ -1,7 +1,7 @@
 const appsList = [
     {title: 'Sesh', description: 'New tab extension for Google Chrome', link: '/sesh'},
     {title: 'Player of words', description: 'practice english word by word', link: '/player-of-words'},
-    //{title: 'Player of numbers', description: 'Practice Math', link: '/player-of-numbers'},
+    {title: 'Player of numbers', description: 'Practice Math', link: '/player-of-numbers'},
     {title: 'Darktionary', description: 'Dictionary, but dark', link: '/darktionary'},
     {title: 'YTG', description: 'Youtube Thumbnail grabber', link: '/ytgrabber'},
     {title: 'Game of life', description: "A simple conway's game of life simulation with react", link: '/gameoflife'},
