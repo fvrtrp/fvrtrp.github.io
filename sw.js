1e6885e7fba6ecda00353c051e69cc9756fb0c0f
surajk95 <surajkukati95@gmail.com>
2022-09-08 01:58:39 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-de701c46c97bb9a635f2.js"
+    "url": "webpack-runtime-09da6d582613a7168c90.js"
   },
   {
     "url": "framework-256462003f1c550867f6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b75609ae66aa44db074d76c1dcee6d21"
+    "revision": "4a349bf34c4d689c04dc7136a3b68292"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7542ec632975394c09bc.js"
